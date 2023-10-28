3e9b584b459683e3d8c1996f3836bf0722cbd7bc
Misha <mishagrigoryan@gmail.com>
2023-10-28 14:49:55 -0400

    app

--- app.py
+++ app.py
@@ -16,4 +16,21 @@ CREATE TABLE IF NOT EXISTS transactions (
 '''
 conn.execute(create_table_query)
 
+import pandas as pd
 
+data = pd.read_csv('online_retail_ii.csv')
+data.dropna(inplace=True) 
+
+
+data['InvoiceDate'] = pd.to_datetime(data['InvoiceDate'])
+data.to_sql('transactions', conn, if_exists='replace', index=False)
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+plt.figure(figsize=(10, 6))
+data['InvoiceDate'].value_counts().plot(kind='line')
+plt.title('Number of Transactions Over Time')
+plt.xlabel('Date')
+plt.ylabel('Number of Transactions')
+plt.show()
